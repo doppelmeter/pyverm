26a87093f8eeeedc357029c1dc059147215f27be
Marius Hürzeler <huerzeler.marius@gmail.com>
2018-12-14 23:27:11 +0100

    adds circle

---
+++ pyverm/core/circle.py
@@ -0,0 +1,45 @@
+import decimal
+from .points import Point
+
+
+class Circle:
+    """
+    Represents a Circle
+    """
+    def __init__(self,*, center_point=None, radius=None):
+        """
+        initialise a circle object.
+
+        :param center_point: ``Point``-object or ``(y,x)``-tuple
+        :param radius: distance in meter
+        """
+        if radius is not None:
+            self.radius = decimal.Decimal(radius)
+
+        if center_point is Point:
+            self.center_point = center_point
+        elif center_point is not None:
+            self.center_point = Point(center_point[0],center_point[1])
+
+    def from_3_points(self, point_1, point_2, point_3):
+        """
+        Calculates Radius and center_point out of 3 points, and sets the circle to the calculated values.
+
+        **To-To**
+
+        :param point_1: ``Point``-object or ``(y,x)``-tuple
+        :param point_2: ``Point``-object or ``(y,x)``-tuple
+        :param point_3: ``Point``-object or ``(y,x)``-tuple
+        """
+        pass
+
+    def from_2_points_radius(self, point_1, point_2, radius):
+        """
+        Calculates Radius and center_point out of 2 points and the radius, and sets the circle to the calculated values.
+
+        **To-To**
+
+        :param point_1: ``Point``-object or ``(y,x)``-tuple
+        :param point_2: ``Point``-object or ``(y,x)``-tuple
+        :param radius: radius in meter
+        """
