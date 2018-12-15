96c5cf26b16308dc8cff939b3d3eef267b8d93a7
Marius Hürzeler <huerzeler.marius@gmail.com>
2018-12-15 23:53:00 +0100

    Add Licence to all code files

--- pyverm/core/reporting.py
+++ pyverm/core/reporting.py
@@ -1,23 +1,43 @@
-"""Reporting Module
+########################################################################
+#                                                                      #
+# Copyright (C) 2018,  Marius Hürzeler                                 #
+#                                                                      #
+# This file is part of PyVerm.                                         #
+#                                                                      #
+# PyVerm is free software: you can redistribute it and/or modify       #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# PyVerm is distributed in the hope that it will be useful,            #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License for more details.                         #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                      #
+########################################################################
+"""
+Reporting Module
 
 This module defines reporting-engin for pyverm.
 """
 
 __all__ = ["getReport"]
 
+__author__ = "Marius Hürzler"
+__copyright__ = "Copyright (C) 2018, Marius Hürzeler"
+__license__ = "GNU GPLv3"
 
 
-#imports from standard library
 import logging
 
-#imports from pyverm
 from . import settings
 
 
-# Module "configuration"
-#=======================
-# logger
 logger = logging.getLogger(__name__)
+
 
 class _Report:
     def __init__(self):
@@ -51,10 +71,6 @@ class _Report:
 
     def __str__(self):
         return self._report
-
-
-
-
 
 
 #Initialize Report if it doesn't exist
