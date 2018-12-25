0fc6e91627a53a3e976170c2f2e8058bd2c02bab
Marius Hürzeler <huerzeler.marius@gmail.com>
2018-12-25 12:04:17 +0100

    adding unittest for transformations

---
+++ pyverm/tests/test__transformation.py
@@ -0,0 +1,34 @@
+########################################################################
+#                                                                      #
+# Copyright (C) 2018,  Marius Hürzeler                                 #
+#                                                                      #
+# This file is part of PyVerm.                                         #
+#                                                                      #
+# PyVerm is free software: you can redistribute it and/or modify       #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# PyVerm is distributed in the hope that it will be useful,            #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License for more details.                         #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                      #
+########################################################################
+
+import pytest
+
+import decimal
+
+from .. import _transformation
+
+
+class Test_Transformation:
+    def test_transformation_helmert(self):
+        trans = _transformation.Transformation(((0, 0), (1, 1)), ((1, 1), (2, 2)), method="helmert")
+        info = trans.info()
+        var = trans.to_destination((10, 10))
+        assert var == pytest.approx((11, 11), abs=decimal.Decimal(1e-7))
